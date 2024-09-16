728a57cb2afe1189c05619e7804e84931798bd8a
Domagoj <paulic.domagoj@gmail.com>
2024-09-16 22:08:46 +0200

    impl comments service

--- comments/index.ts
+++ comments/index.ts
@@ -1,1 +1,33 @@
-console.log('Hi there');
+import express from 'express';
+import { randomBytes } from 'crypto';
+
+const app = express();
+app.use(express.json());
+
+interface Comment {
+  id: string;
+  content: string;
+}
+
+const commentsByPostId: Record<PropertyKey, Comment[]> = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
