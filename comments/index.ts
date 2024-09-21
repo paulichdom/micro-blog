8b99b984339c20e3b79f85ceabaf7fc31ac62403
Domagoj <paulic.domagoj@gmail.com>
2024-09-21 14:31:02 +0200

    emit comment creation events

--- comments/index.ts
+++ comments/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { randomBytes } from 'crypto';
 import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 app.use(express.json());
@@ -17,7 +18,7 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
@@ -26,6 +27,17 @@ app.post('/posts/:id/comments', (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  await axios
+    .post('http://localhost:4005/events', {
+      type: 'CommentCreated',
+      data: {
+        id: commentId,
+        content,
+        postId: req.params.id,
+      },
+    })
+    .catch((error) => console.log(error));
 
   res.status(201).send(comments);
 });
