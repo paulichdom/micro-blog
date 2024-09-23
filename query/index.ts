8d0ef9b1f34fd0f39fbd8a43fa8efcadf8d6beec
Domagoj <paulic.domagoj@gmail.com>
2024-09-23 21:08:06 +0200

    parse incoming events

--- query/index.ts
+++ query/index.ts
@@ -1,13 +1,35 @@
 import express from 'express';
 import cors from 'cors';
+import { Post } from './types';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {});
+const posts: Record<PropertyKey, Post> = {};
 
-app.post('/events', (req, res) => {});
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(JSON.stringify({ posts }, null, 2));
+
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log('Listening on 4002');
---
+++ query/types.ts
@@ -0,0 +1,10 @@
+export interface Post {
+  id: string;
+  title: string;
+  comments: Comment[];
+}
+
+export interface Comment {
+  id: string;
+  content: string;
+}
