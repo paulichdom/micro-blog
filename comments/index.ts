5bdcd2808b39baf5dd81383eefcd0346b348d79a
Domagoj <paulic.domagoj@gmail.com>
2024-09-24 16:25:34 +0200

    update comment content 46

--- comments/index.ts
+++ comments/index.ts
@@ -10,7 +10,7 @@ app.use(cors());
 interface Comment {
   id: string;
   content: string;
-  status: 'pending' | 'approved' | 'rejected'
+  status: 'pending' | 'approved' | 'rejected';
 }
 
 const commentsByPostId: Record<PropertyKey, Comment[]> = {};
@@ -44,8 +44,29 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log({ RecievedEvent: req.body.type });
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => comment.id === id)!;
+
+    comment.status = status;
+
+    await axios.post('http://localhost:4005', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
 
   res.send({});
 });
